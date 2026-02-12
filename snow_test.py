b825e5e2938b75c39129db9d4e0d14bb4f0dbbc7
hjaltd <hdu@mpe.au.dk>
2026-02-12 17:10:45 +0200

    Update snow_test.py

--- snow_test.py
+++ snow_test.py
@@ -24,19 +24,20 @@ test_snow_dark_arr = test_snow_arr[:,0:960,:]
 test_snow_light_arr = test_snow_arr[:,961:1920,:]
 
 # Analyze statistical properties of the output image
+# mean of a Poisson distribution should equal its variance (std^2)
 mean_dark  = np.mean(test_snow_dark_arr)
 mean_light = np.mean(test_snow_light_arr)
 std_dark  = np.std(test_snow_dark_arr,mean=mean_dark,ddof=1)
 std_light = np.std(test_snow_light_arr,mean=mean_light,ddof=1)
 
-print(f"Dark pixels: {mean_dark} +/- {std_dark}")
-print(f"Light pixels: {mean_light} +/- {std_light} ")
+print(f"Dark pixels: mean {mean_dark}, variance {std_dark**2}")
+print(f"Light pixels: mean {mean_light}, variance {std_light**2}")
 
 # Plot histograms
 dark_count, dark_bins = np.histogram(test_snow_dark_arr,bins=256)
 light_count, light_bins = np.histogram(test_snow_light_arr,bins=256)
 
-plt.figure()
-plt.stairs(dark_count,dark_bins)
-plt.stairs(light_count,light_bins)
-plt.show()
+# plt.figure()
+# plt.stairs(dark_count,dark_bins)
+# plt.stairs(light_count,light_bins)
+# plt.show()
